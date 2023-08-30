d4e196d5447163d0ac0defe23c5e4e039a364c71
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-08-30 17:56:37 -0300

    Creacion de models/Usuario en Shared

---
+++ Motored/Shared/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motored.Shared.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public double DNI { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public string ConfirmarContraseña { get; set;} 
+
+        public string Pais { get; set;  } 
+
+        public string Provincia { get; set;}
+
+    }
+}
