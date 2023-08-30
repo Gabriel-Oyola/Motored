b55ed98b30fd54329431662cbe56156bb7bad7ac
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-08-30 18:16:05 -0300

    Creacion de Models/opiniones dentro de Shared

---
+++ Motored/Shared/Models/Opiniones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motored.Shared.Models
+{
+    public class Opiniones
+    {
+
+        public int IdUsuario{ get; set; } 
+
+        public int IdTaller { get; set; }
+
+        public int IdOpinion { get; set; }
+
+        public string Comentario { get; set; } 
+
+        public DateTime Fecha { get; set; }
+
+        public int Estrellas { get; set; }
+
+
+    }
+}
