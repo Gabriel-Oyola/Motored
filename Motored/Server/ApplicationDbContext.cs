608fb9015ac9003f666933317df178d9f502d60e
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-08-30 18:33:20 -0300

    Se creo un archivo DbContext con sus Tablas

---
+++ Motored/Server/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Motored.Shared.Models;
+
+namespace Motored.Server
+{
+    public class ApplicationDbContext:DbContext
+    { 
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        
+        } 
+
+        public DbSet<Usuario>usuarios { get; set; } 
+       
+        public DbSet<Taller> talleres { get; set; } 
+
+        public DbSet<TallerTop> talleresTop { get; set; } 
+
+        public DbSet<ReporteOpiniones>reporteOpiniones { get; set; }
+        public DbSet<Opiniones> opiniones { get; set; }
+
+        public DbSet<Motocicletas> motocicletas { get; set; }
+        
+
+
+
+    }
+}
