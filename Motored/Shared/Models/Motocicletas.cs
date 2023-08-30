fd3973f7042af487a030d8b2d3c24cd12907598b
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-08-30 18:12:28 -0300

    Creacion de Models/Motocicletas dentro de shared

---
+++ Motored/Shared/Models/Motocicletas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motored.Shared.Models
+{
+    public  class Motocicletas
+    { 
+
+        public int IdUsuario { get; set; } 
+        public int IdMoto { get; set; } 
+
+        public string Patente { get; set; } 
+
+        public string Marca { get; set; } 
+
+        public string Modelo { get; set;} 
+        
+        public string Año { get; set; } 
+
+        public string Aseguradora { get; set; } 
+
+
+    }
+}
