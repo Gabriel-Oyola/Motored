1545444bb3f3e2d3d924f55efb1224aff8ab46b9
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-08-30 18:24:36 -0300

    Creacion de Models/ReporteOpiniones dentro de shared

---
+++ Motored/Shared/Models/ReporteOpiniones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motored.Shared.Models
+{
+    public class ReporteOpiniones
+    {
+        public int IdUsuarioRC { get; set; }
+        public int IdTaller { get; set; } 
+
+        public int IdOpinion { get; set; }
+
+        public string Comentario { get; set; }
+
+        public DateTime Fecha { get; set; } 
+
+        public string Estrellas { get; set; }
+
+        public int IdReporte { get; set; }
+
+        public string Motivo { get; set; } 
+
+        public string Sancion { get; set; } 
+
+
+
+    }
+}
